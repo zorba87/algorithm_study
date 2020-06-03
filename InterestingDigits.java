3396cc0b1ec2b79e7ca63f3b0763bfdcfbbb0ce4
zorba Lee <kulkulzaza@gmail.com>
2020-06-04 01:32:54 +0900

    solv. InterestingDigits.java

--- InterestingDigits.java
+++ InterestingDigits.java
@@ -1,5 +1,6 @@
-package hblee;
+package topcoder;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InterestingDigits {
@@ -11,43 +12,36 @@ public class InterestingDigits {
 		int base = sc.nextInt();
 		int [] result = id.digits(base);
 		
-//		for(int ret : result) {
-//			System.out.println(ret);
-//		}
+		for(int ret : result) {
+			System.out.println(ret);
+		}
 	}
 	
 	public int[] digits(int base) {
-		int[] result = null;
-
-		int sumArr[] = new int[1001];
 		
-		StringBuffer sb;
-
-		for(int i =1; i<1000; i++) {
-			int sum =0;
-			int remain=0;
-			int temp; 
-			sb = new StringBuffer();
+		ArrayList<Integer> list = new ArrayList<>();
 		
-			int val = i;
-			while(true) {
-				remain = val % base;
-				val = val / base;
-				
-				sb.append(remain);
-				sum+=remain;
-				
-				if(val < base) {
-					sum += val;
-					sb.append(val);
-					break;
-				}
-			}
-			System.out.print(i +" ==== ");
-			System.out.println(sb.reverse().toString());
+		for(int i=2; i< base; i++) {
+			boolean ok = true;
 			
-		}
+			for(int k1=0; k1 < base; k1++) {
+				for(int k2=0; k2 <base; k2++) {
+					for(int k3=0;k3<base;k3++) {
+						if((k3+k2*base+k1*base*base) % i ==0 && (k1+k2+k3) % i != 0) {
+							ok = false;
+							break;
+						}
+					}
+					if(!ok) break;
+				}
+				if(!ok) break;
+			}
+			if(ok) list.add(i); 
+		}		
 		
-		return result; 
+		int ans[] = new int[list.size()];
+		for(int i=0; i< list.size();i++) ans[i] = list.get(i);
+		
+		return ans; 
 	}
 }
